085070cac3927ba6d284db18e6da0d0f2e2e2a72
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-07-28 10:35:17 +0700

    Update New_Cust_Seg.py

--- New_Cust_Seg.py
+++ New_Cust_Seg.py
@@ -19,11 +19,13 @@ from numpy import arange
 from bokeh.models.widgets import Div
 
 st.markdown('''
-# **Customer Segmentation**
-**Customer Segmentation** is the process of division of a customer base into several groups.
-''')
+# **Customer Segmentation**''')
 
 st.image("audience-segmentation-concept-man-near-a-large-circular-chart-with-images-of-people-illustration-flat-vector.jpg")
+
+st.markdown('''
+## **Customer Segmentation** is the process of division of a customer base into several groups.
+''')
 
 st.write("📊  These groups share similarities that are relevant to marketing such as gender, age, annual income and spending habits.")
 st.write("📊  Once your company understands the characteristics of these 'clusters' of clients you can divert your ad budget away from those who are unlikely to purchase your product or service towards your most valuable customers")
