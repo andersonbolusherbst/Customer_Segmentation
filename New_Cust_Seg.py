d4bf48bdd24cf373e08d18f8612bc8d511288963
Bradley M Bolus <80377120+bradbolus@users.noreply.github.com>
2022-07-28 11:10:31 +0700

    Update New_Cust_Seg.py

--- New_Cust_Seg.py
+++ New_Cust_Seg.py
@@ -21,12 +21,12 @@ st.markdown('''
 st.image("audience-segmentation-concept-man-near-a-large-circular-chart-with-images-of-people-illustration-flat-vector.jpg")
 
 st.markdown('''
-##### Customer Segmentation is the process of division of a customer base into several groups.
+#### ✔️ Customer Segmentation is the process of division of a customer base into several groups.
 ''')
 
-st.markdown('''##### ✔️ These groups share similarities that are relevant to marketing such as gender, age, annual income and spending habits.''')
-st.markdown('''##### ✔️ Once your company understands the characteristics of these 'clusters' of clients you can divert your ad budget away from those who are unlikely to purchase your product or service towards your most valuable customers''')
-st.markdown('''##### ✔️ This customer segmentation will be completed on customer data from the popular MALL DATASET on Kaggle''')
+st.markdown('''#### ✔️ These groups share similarities that are relevant to marketing such as gender, age, annual income and spending habits.''')
+st.markdown('''#### ✔️ Once your company understands the characteristics of these 'clusters' of clients you can divert your ad budget away from those who are unlikely to purchase your product or service towards your most valuable customers''')
+st.markdown('''#### ✔️ This customer segmentation will be completed on customer data from the popular MALL DATASET on Kaggle''')
 
 
 if st.button('Press me for Customer Segmentation'):
